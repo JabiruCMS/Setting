d67ee6550e91cc40a099507c6235e3a57f2b17ee
David Cap <david.cap@bocians.net>
2022-10-18 14:09:03 -0600

    optimize settings helper

--- helpers.php
+++ helpers.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
+
 if (! function_exists('setting')) {
     function setting($name, $locale = null, $default = null)
     {
-        return app('setting.settings')->get($name, $locale, $default);
+        return Cache::driver('array')
+            ->remember('setting_' . sha1($name . (string)$locale), 60, function () use ($name, $locale, $default) {
+                return app('setting.settings')->get($name, $locale, $default);
+            });
     }
 }
